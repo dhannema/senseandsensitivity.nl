565be5a86351eca2e29ca0fe5fb0bb65eaa21750
Don Hannema <don.hannema@gmail.com>
2018-03-14 10:48:41 +0100

    Auto commit

---
+++ wordpress.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent --no-check-certificate --cut-dirs=0 --no-host-directories https://senseandsensitivity.nl
