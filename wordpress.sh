0bec3937b94ef033e80812fb82f35032a57f4e27
Don Hannema <don.hannema@gmail.com>
2018-03-14 10:50:20 +0100

    Auto commit

--- wordpress.sh
+++ wordpress.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 wget --mirror --convert-links --adjust-extension --page-requisites --no-parent --no-check-certificate --cut-dirs=0 --no-host-directories https://senseandsensitivity.nl
+
+git add .
+git commit -am "Auto commit"
+git push
