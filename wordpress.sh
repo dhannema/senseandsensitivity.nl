c4f2b967641063bb1a5954f9d612878860cb472a
dhannema <don.hannema@gmail.com>
2018-03-14 12:00:09 +0100

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -2,7 +2,7 @@
 
 git pull
 
-wget --mirror --convert-links --page-requisites --no-parent --no-check-certificate --cut-dirs=0 --no-host-directories https://senseandsensitivity.nl
+wget --mirror --restrict-file-names=windows --convert-links --page-requisites --no-parent --no-check-certificate --cut-dirs=0 --no-host-directories https://senseandsensitivity.nl
 
 git add .
 git commit -am "Auto commit"
