9ab97ee1c862fff0947f557745c45045b50d0cae
Don Hannema <don.hannema@gmail.com>
2018-03-29 15:32:55 +0200

    Auto commit

--- wordpress.sh
+++ wordpress.sh
@@ -4,6 +4,8 @@ git pull
 
 wget --mirror --restrict-file-names=windows --html-extension --convert-links --page-requisites --no-parent --no-check-certificate --cut-dirs=0 --no-host-directories https://senseandsensitivity.nl
 
+git config --global user.email "don.hannema@gmail.com"
+git config --global user.name "Don Hannema"
 git add .
 git commit -am "Auto commit"
 git push
