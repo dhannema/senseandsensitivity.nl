2517b71bdec19cff864c5a7ba3f336a962c73523
Don Hannema <don.hannema@gmail.com>
2018-04-09 06:00:02 +0200

    Auto commit

--- wordpress.sh
+++ wordpress.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# $ nano /etc/config/crontab    
+# 0 6 * * * cd /share/Don/senseandsensitivity.nl/ && ./wordpress.sh > wordpress.log 2>&1
+# $ crontab /etc/config/crontab 
+# $ /etc/init.d/crond.sh restart
+
 exe() { 
     if [ "$DRY_RUN" == "" ]; then
         echo "`date` \$ $@" ; "$@" ; 
@@ -12,7 +17,6 @@ exe /opt/bin/git pull
 
 exe /usr/bin/wget -O --mirror --restrict-file-names=windows --html-extension --convert-links --page-requisites --no-parent --no-check-certificate --cut-dirs=0 --no-host-directories https://senseandsensitivity.nl
 
-exe /opt/bin/git status
 exe /opt/bin/git config user.email "don.hannema@gmail.com"
 exe /opt/bin/git config user.name "Don Hannema"
 exe /opt/bin/git add .
