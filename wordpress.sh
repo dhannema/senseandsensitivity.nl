24867b0783af26949cbac5bc0294dc6869ceae95
Don Hannema <don.hannema@gmail.com>
2018-04-11 06:00:02 +0200

    Auto commit

--- wordpress.sh
+++ wordpress.sh
@@ -17,6 +17,8 @@ exe /opt/bin/git pull
 
 exe /usr/bin/wget -O --mirror --restrict-file-names=windows --html-extension --convert-links --page-requisites --no-parent --no-check-certificate --cut-dirs=0 --no-host-directories https://senseandsensitivity.nl
 
+exe sed -i "s/\&copy 2018 Sense \& Sensitivity/\&copy 2018 Sense \& Sensitivity [Maintenance Mode]/" index.html
+
 exe /opt/bin/git config user.email "don.hannema@gmail.com"
 exe /opt/bin/git config user.name "Don Hannema"
 exe /opt/bin/git add .
